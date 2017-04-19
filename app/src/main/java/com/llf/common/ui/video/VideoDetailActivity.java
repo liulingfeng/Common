b34bfa3b3553ac46373cf7ff63b1edb78b78269b
liulf <854034241@qq.com>
2017-04-19 15:30:01 +0800

    修改视频详情页

--- app/src/main/java/com/llf/common/ui/video/VideoDetailActivity.java
+++ app/src/main/java/com/llf/common/ui/video/VideoDetailActivity.java
@@ -2,8 +2,12 @@ package com.llf.common.ui.video;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.llf.basemodel.base.BaseActivity;
+import com.llf.basemodel.dialog.ShareDialog;
 import com.llf.common.R;
 
 import butterknife.Bind;
@@ -17,13 +21,16 @@ import llf.videomodel.VideoPlayer;
 public class VideoDetailActivity extends BaseActivity {
     @Bind(R.id.viewPager)
     VideoPlayer mViewPager;
+    @Bind(R.id.toolbar)
+    Toolbar mToolbar;
 
-    public static void launch(Context context,String url, String title){
-        Intent intent = new Intent(context,VideoDetailActivity.class);
-        intent.putExtra("url",url);
-        intent.putExtra("title",title);
+    public static void launch(Context context, String url, String title) {
+        Intent intent = new Intent(context, VideoDetailActivity.class);
+        intent.putExtra("url", url);
+        intent.putExtra("title", title);
         context.startActivity(intent);
     }
+
     @Override
     protected int getLayoutId() {
         return R.layout.activity_video_detail;
@@ -33,6 +40,37 @@ public class VideoDetailActivity extends BaseActivity {
     protected void initView() {
         Intent intent = getIntent();
         mViewPager.playVideo(intent.getStringExtra("url"), intent.getStringExtra("title"));
+
+        mToolbar.setContentInsetStartWithNavigation(0);
+        mToolbar.inflateMenu(R.menu.menu_video_detail);
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        mToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.search:
+                        showToast("搜索");
+                        break;
+                    case R.id.love:
+                        showToast("喜欢");
+                        break;
+                    case R.id.share:
+                        ShareDialog.show(VideoDetailActivity.this);
+                        break;
+                    case R.id.report:
+                        showToast("举报成功");
+                        break;
+                    default:
+                        break;
+                }
+                return false;
+            }
+        });
     }
 
     @Override
