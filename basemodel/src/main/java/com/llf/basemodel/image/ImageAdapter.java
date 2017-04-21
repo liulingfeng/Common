691a3bf753f238b40404c689276c0cf22110d1c9
liulf <854034241@qq.com>
2017-04-21 10:44:30 +0800

    加入大图浏览单击退出

--- basemodel/src/main/java/com/llf/basemodel/image/ImageAdapter.java
+++ basemodel/src/main/java/com/llf/basemodel/image/ImageAdapter.java
@@ -1,5 +1,6 @@
 package com.llf.basemodel.image;
 
+import android.app.Activity;
 import android.content.Context;
 import android.support.v4.view.PagerAdapter;
 import android.view.Gravity;
@@ -8,15 +9,19 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ProgressBar;
+
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.load.resource.drawable.GlideDrawable;
 import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.target.Target;
 import com.llf.basemodel.R;
+
 import java.util.ArrayList;
 import java.util.List;
+
 import uk.co.senab.photoview.PhotoView;
+import uk.co.senab.photoview.PhotoViewAttacher;
 
 /**
  * Created by llf on 2016/10/20.
@@ -27,7 +32,7 @@ public class ImageAdapter extends PagerAdapter {
     private LayoutInflater inflater;
     private Context context;
 
-    public ImageAdapter(Context context, List<String> datas){
+    public ImageAdapter(Context context, List<String> datas) {
         this.context = context;
         this.inflater = LayoutInflater.from(context);
         this.datas = datas;
@@ -35,7 +40,7 @@ public class ImageAdapter extends PagerAdapter {
 
     @Override
     public int getCount() {
-        if(datas == null) return 0;
+        if (datas == null) return 0;
         return datas.size();
     }
 
@@ -43,14 +48,14 @@ public class ImageAdapter extends PagerAdapter {
     @Override
     public Object instantiateItem(ViewGroup container, final int position) {
         View view = inflater.inflate(R.layout.item_pager_image, container, false);
-        if(view != null){
+        if (view != null) {
             final PhotoView imageView = (PhotoView) view.findViewById(R.id.image);
             final ProgressBar loading = new ProgressBar(context);
             FrameLayout.LayoutParams loadingLayoutParams = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
                     ViewGroup.LayoutParams.WRAP_CONTENT);
             loadingLayoutParams.gravity = Gravity.CENTER;
             loading.setLayoutParams(loadingLayoutParams);
-            ((FrameLayout)view).addView(loading);
+            ((FrameLayout) view).addView(loading);
             final String imgurl = datas.get(position);
             loading.setVisibility(View.VISIBLE);
 
@@ -72,6 +77,13 @@ public class ImageAdapter extends PagerAdapter {
                         }
                     })
                     .into(imageView);
+
+            imageView.setOnPhotoTapListener(new PhotoViewAttacher.OnPhotoTapListener() {
+                @Override
+                public void onPhotoTap(View view, float x, float y) {
+                    ((Activity) context).finish();
+                }
+            });
 
             container.addView(view, 0);
         }
