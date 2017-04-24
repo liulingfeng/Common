aa1a76af0071a7b58761b43b834fecf44cb8ac36
liulf <854034241@qq.com>
2017-04-24 14:08:13 +0800

    加入新闻item点击水波涟漪效果

--- app/src/main/java/com/llf/common/ui/news/NewsFragment.java
+++ app/src/main/java/com/llf/common/ui/news/NewsFragment.java
@@ -2,6 +2,8 @@ package com.llf.common.ui.news;
 
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.llf.basemodel.base.BaseFragment;
 import com.llf.basemodel.base.BaseFragmentAdapter;
@@ -9,6 +11,7 @@ import com.llf.common.R;
 import com.llf.common.ui.news.classfi.NewsClassfiFragment;
 
 import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * Created by llf on 2017/3/15.
@@ -25,6 +28,8 @@ public class NewsFragment extends BaseFragment {
     TabLayout mTabs;
     @Bind(R.id.viewPager)
     ViewPager mViewPager;
+    @Bind(R.id.toolbar)
+    Toolbar mToolbar;
 
     private String[] titles = {"头条", "NBA", "汽车", "笑话"};
     private BaseFragment[] mFragments;
@@ -51,10 +56,23 @@ public class NewsFragment extends BaseFragment {
         mAdapter = new BaseFragmentAdapter(getChildFragmentManager(), mFragments, titles);
         mViewPager.setAdapter(mAdapter);
         mTabs.setupWithViewPager(mViewPager);
+
+        mToolbar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast("点击了");
+            }
+        });
     }
 
     @Override
     protected void lazyFetchData() {
 
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
     }
 }
