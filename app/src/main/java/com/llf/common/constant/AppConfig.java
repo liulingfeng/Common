41432aa4ba23c801e75c5cfd167bf4b4a6c10ee8
liulf <854034241@qq.com>
2017-04-26 16:02:35 +0800

    加入分享

---
+++ app/src/main/java/com/llf/common/constant/AppConfig.java
@@ -0,0 +1,12 @@
+package com.llf.common.constant;
+
+/**
+ * Created by llf on 2017/4/26.
+ */
+
+public class AppConfig {
+    /**
+     * 微信appid
+     */
+    public static  String APP_ID = "wxd298b23025a818e9";
+}
---
+++ app/src/main/java/com/llf/common/wxapi/WXEntryActivity.java
@@ -0,0 +1,24 @@
+package com.llf.common.wxapi;
+
+import android.support.v7.app.AppCompatActivity;
+
+import com.tencent.mm.sdk.openapi.BaseReq;
+import com.tencent.mm.sdk.openapi.BaseResp;
+import com.tencent.mm.sdk.openapi.IWXAPIEventHandler;
+
+/**
+ * Created by llf on 2017/4/26.
+ * 分享回调页
+ */
+
+public class WXEntryActivity extends AppCompatActivity implements IWXAPIEventHandler {
+    @Override
+    public void onReq(BaseReq baseReq) {
+
+    }
+
+    @Override
+    public void onResp(BaseResp baseResp) {
+
+    }
+}
