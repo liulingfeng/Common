223c60258b080ab53dab74db2e9367f944e7aaef
liulf <854034241@qq.com>
2017-05-05 14:03:19 +0800

    适配7.0

--- app/src/main/java/com/llf/common/receiver/InstallReceiver.java
+++ app/src/main/java/com/llf/common/receiver/InstallReceiver.java
@@ -25,43 +25,9 @@ public class InstallReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         if (intent.getAction().equals(DownloadManager.ACTION_DOWNLOAD_COMPLETE)) {
             LogUtil.d(TAG + "收到下载完成的广播");
-            //下载失败也会发送下载完成
-//            long downloadApkId = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1);
-//            installApk(context, downloadApkId);
             install(context);
         }
     }
-
-    // 安装Apk
-//    private void installApk(Context context, long downloadApkId) {
-//        // 获取存储ID
-//        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
-//        long id = sp.getLong(DownloadUtil.DOWNLOAD_APK_ID_PREFS, -1L);
-//
-//        if (downloadApkId == id) {
-//            DownloadManager manager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
-//            //打开一个下载完成的任务
-//            Intent install = new Intent(Intent.ACTION_VIEW);
-//            Uri downloadFileUri = manager.getUriForDownloadedFile(downloadApkId);
-//            if (downloadFileUri != null) {
-//                try {
-//                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-//                        install.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-//                    } else {
-//                        install.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-//                    }
-//                    install.setDataAndType(downloadFileUri, "application/vnd.android.package-archive");
-//                    context.startActivity(install);
-//                } catch (Exception e) {
-//                    LogUtil.e(TAG + "没有这个Activity");
-//                }
-//            } else {
-//                manager.remove(id);
-//            }
-//        }
-//    }
-
-
     /**
      * 通过隐式意图调用系统安装程序安装APK
      * 适配了7.0
