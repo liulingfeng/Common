d4bf976625ae6038c2078050f22458c0633d76eb
liulf <854034241@qq.com>
2017-06-28 09:55:02 +0800

    添加混淆

--- app/src/main/java/com/llf/common/App.java
+++ app/src/main/java/com/llf/common/App.java
@@ -9,14 +9,36 @@ import com.llf.basemodel.base.BaseApplication;
  * Created by llf on 2017/3/10.
  */
 
-public class App extends BaseApplication{
+public class App extends BaseApplication {
     @Override
     public void onCreate() {
         super.onCreate();
+        /**
+         * 解决7.0无法使用file://格式的URI的第二种方法
+         */
         StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
         StrictMode.setVmPolicy(builder.build());
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
             builder.detectFileUriExposure();
+        }
+    }
+
+    /**
+     * 内存紧张时会走这个方法
+     *
+     * @param level
+     */
+    @Override
+    public void onTrimMemory(int level) {
+        super.onTrimMemory(level);
+        //1.清理内存中的图片2.清理掉Activity只保留Root Activity
+        switch (level){
+            case TRIM_MEMORY_COMPLETE:
+                //表示 App 退出到后台，并且已经处于 LRU List 比较考靠前的位置
+                break;
+            case TRIM_MEMORY_RUNNING_CRITICAL:
+                //表示 App 正在正常运行，但是系统已经开始根据 LRU List 的缓存规则杀掉了一部分缓存的进程
+                break;
         }
     }
 }
